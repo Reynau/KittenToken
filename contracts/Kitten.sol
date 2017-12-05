b76d2f13467348082d0d6e49393ffaebb01a489f
Reynau <xavi.lopez.reynau@gmail.com>
2017-12-05 10:00:14 +0100

    Kitten.sol include and inherit from ERC20.sol

--- contracts/Kitten.sol
+++ contracts/Kitten.sol
@@ -1,14 +1,6 @@
 pragma solidity ^0.4.0;
 
-contract ERC20 {
-    function balanceOf(address _owner) public constant returns (uint256 balance);
-    function transfer(address _to, uint256 _amount) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success);
-    function approve(address _spender, uint256 _amount) public returns (bool success);
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
-}
+import './ERC20.sol';
 
 contract Kitten is ERC20 {
 
