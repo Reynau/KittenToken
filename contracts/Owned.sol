9660a2fbe7b79f1f347767de9bb4b9a6497b2d90
Reynau <xavi.lopez.reynau@gmail.com>
2017-12-05 10:49:42 +0100

    Add Owned contract

---
+++ contracts/Owned.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+
+contract Owned {
+    function Owned(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
