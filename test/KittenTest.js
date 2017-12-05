fe9a38c8b922006ff319ac4f97bf2e51311c1511
Reynau <xavi.lopez.reynau@gmail.com>
2017-12-05 10:42:13 +0100

    Fix transactions testing

--- test/KittenTest.js
+++ test/KittenTest.js
@@ -1,16 +1,26 @@
 var Kitten = artifacts.require("Kitten");
 
 contract('Kitten', function(accounts){
-  it("Should use first account as creator", function () {
-    return Kitten.deployed().then(function (instance) {
+  var kitten;
+  var initialSupply = 100;
+  it("Should use first account as owner", function () {
+    return Kitten.new(initialSupply).then(function (instance) {
       return instance.owner.call();
     }).then(function (owner) {
       assert.equal(owner, accounts[0], "The owner is not the first account")
     });
   });
 
+  it("Should return the initial supply", function () {
+    return Kitten.new(initialSupply).then(function (instance) {
+      return instance.balanceOf.call(accounts[0]);
+    }).then(function (balance) {
+      assert.equal(balance, initialSupply, "Not returning initial supply");
+    })
+  });
+
   it("Should return empty balance", function () {
-    return Kitten.deployed().then(function (instance) {
+    return Kitten.new(initialSupply).then(function (instance) {
       return instance.balanceOf.call(accounts[1]);
     }).then(function (balance) {
       assert.equal(balance, 0, "Not returning empty balance");
@@ -18,37 +28,55 @@ contract('Kitten', function(accounts){
   });
 
   it("Should return correct balance", function () {
-    return Kitten.deployed().then(function (instance) {
-      if (instance.transfer(accounts[1], 25)) return instance.balanceOf.call(accounts[1]);
+    var transferAmount = 25;
+    return Kitten.new(initialSupply).then(function (instance) {
+      if (instance.transfer(accounts[1], transferAmount)) return instance.balanceOf.call(accounts[1]);
       else assert.fail("Transfer not done");
     }).then(function (balance) {
-      assert.equal(balance, 25, "Not returning correct balance");
+      assert.equal(balance, transferAmount, "Not returning correct balance");
     })
   });
 
   it("Should approve to use tokens to another account", function () {
-    return Kitten.deployed().then(function (instance) {
-      if (instance.approve(accounts[1], 30)) {
-        return instance.allowance.call(accounts[0], accounts[1]);
-      } else {
-        assert.fail("Doesn't approve properly")
-      }
+    var approveAmount = 30;
+    return Kitten.new(initialSupply).then(function (instance) {
+      kitten = instance;
+
+      return instance.approve(accounts[1], approveAmount);
+    }).then(function(results) {
+      assert.equal(results.logs.length, 1); // Just one event triggered
+
+      var approveEvent = results.logs[0];
+      assert.equal(approveEvent.args._owner, accounts[0], "Event owner is not correct");
+      assert.equal(approveEvent.args._spender, accounts[1], "Event spender is not correct");
+      assert.equal(approveEvent.args._amount, approveAmount, "Event amount is not correct");
+
+      return kitten.allowance.call(accounts[0], accounts[1]);
     }).then(function(remaining) {
-      assert.equal(remaining, 30, "Allowance not equal to approved amount");
+      assert.equal(remaining, approveAmount, "Allowance not equal to approved amount");
     });
   });
 
   it("Should transfer with allowance", function() {
-    return Kitten.deployed().then(function (instance) {
-      return { approved: instance.approve.call(accounts[1], 30), instance: instance };
-    }).then(function(data) {
-      if (data.approved) return { approved: data.instance.transferFrom.call(accounts[0], accounts[1], 25), instance: data.instance };
-      else assert.fail("Allowance not approved");
-    }).then(function(data) {
-      if (data.approved) return data.instance.balanceOf.call(accounts[1]);
-      else assert.fail("Transfer not done");
+    var approveAmount = 30;
+    var transferAmount = 25;
+    return Kitten.new(initialSupply).then(function (instance) {
+      kitten = instance;
+
+      if (instance.approve(accounts[1], approveAmount))
+        return instance.transferFrom(accounts[0], accounts[1], transferAmount, {from: accounts[1]});
+      else assert.fail("Failed to approve or return value is not valid");
+    }).then(function(results) {
+      assert.equal(results.logs.length, 1); // Just one event triggered
+
+      var approveEvent = results.logs[0];
+      assert.equal(approveEvent.args._from, accounts[0], "Event owner is not correct");
+      assert.equal(approveEvent.args._to, accounts[1], "Event spender is not correct");
+      assert.equal(approveEvent.args._amount, transferAmount, "Event amount is not correct");
+
+      return kitten.balanceOf.call(accounts[1]);
     }).then(function(balance) {
-      assert.equal(balance, 25, "Not returning correct balance");
+      assert.equal(balance, transferAmount, "Not returning correct balance");
     });
   });
 });
